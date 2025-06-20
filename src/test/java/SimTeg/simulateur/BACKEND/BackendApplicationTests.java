1bb6416299d7202bd58be31c8114b713f3f585d4
brenda tonfack <brendaelvira183@gmail.com>
2025-06-20 18:11:58 +0100

    les modifications

--- src/test/java/SimTeg/simulateur/BACKEND/BackendApplicationTests.java
+++ src/test/java/SimTeg/simulateur/BACKEND/BackendApplicationTests.java
@@ -1,13 +1,32 @@
+
 package SimTeg.simulateur.BACKEND;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import SimTeg.simulateur.BACKEND.Repository.RoleRepository;
+import SimTeg.simulateur.BACKEND.User.Role;
 
-@SpringBootTest
-class BackendApplicationTests {
+@SpringBootApplication
+@EnableJpaAuditing
+@EnableAsync
+public class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository){
+		return args -> {
+			if (roleRepository.findByName("ADMIN").isEmpty()){
+				roleRepository.save(Role.builder()
+						.name("ADMIN")
+						.build());
+			}
+		};
+	}
 }
